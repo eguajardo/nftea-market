a026d81257ab168d494cf37247d5a74b9329a362
Edwin Guajardo <edwin.guajardo@gmail.com>
2021-10-13 23:29:08 -0500

    Added initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-nfteas-cafe
+# NFTeas.cafe
+
+## Environment
+
+First we install the dependencies
+
+```
+npm install
+```
+
+Create a .env file in the root folder with the following variables and replace the values with your test blockchain accounts private keys. It's recommended to use different accounts than the ones you use for mainnet.
+
+```
+TESTNET_ACCOUNT_1=[private key 1]
+TESTNET_ACCOUNT_2=[private key 2]
+TESTNET_ACCOUNT_3=[private key 3]
+TESTNET_ACCOUNT_4=[private key 4]
+TESTNET_ACCOUNT_5=[private key 5]
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-waffle";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.3", settings: {} }],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://rpc-mumbai.maticvigil.com",
+      },
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        process.env.TESTNET_ACCOUNT_1!,
+        process.env.TESTNET_ACCOUNT_2!,
+        process.env.TESTNET_ACCOUNT_3!,
+        process.env.TESTNET_ACCOUNT_4!,
+        process.env.TESTNET_ACCOUNT_5!,
+      ],
+    },
+  },
+};
+export default config;
