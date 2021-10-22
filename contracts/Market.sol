4ab8214a88f775aa5e2a557da13b8caa377548da
Edwin Guajardo <edwin.guajardo@gmail.com>
2021-10-21 21:30:59 -0500

    Added functionality to post new token for sale in smart contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "./MultiToken.sol";
 
 /**
@@ -9,6 +10,8 @@ import "./MultiToken.sol";
  * @notice Contract handling stalls and purchases
  */
 contract Market is Context {
+
+    uint8 public constant MINIMUM_NFT_PRICE = 1;
 
     /**
      * @dev ERC-1155 token contract handling the NFTs for sale
@@ -21,9 +24,24 @@ contract Market is Context {
     mapping (address => string) private _stallNames;
 
     /**
+     * @dev Mapping of stall name to it's owner
+     */
+    mapping (string => address) private _vendors;
+
+    /**
      * @dev Mapping of stall names to their metadata URI
      */
     mapping (string => string) private _uris;
+
+    /**
+     * @dev Mapping of stall names to an array of the token classes for sale
+     */
+    mapping (string => uint128[]) private _stallTokens;
+
+    /**
+     * @dev Mapping of the token class to it's price
+     */
+    mapping (uint128 => uint256) private _tokenPrices;
 
     /**
      * @notice Emitted when the sender `vendor` registers a stall with the
@@ -39,6 +57,21 @@ contract Market is Context {
     }
 
     /**
+     * @dev Modifier that checks the sender is a registered vendor
+     */
+    modifier onlyVendor() {
+        require(_vendorRegistered(_msgSender()), "Market: account is not a registered vendor");
+        _;
+    }
+
+    /**
+     * @notice Returns the address of the ERC1155 contract address
+     */
+    function tokenContractAddress() external view returns (address) {
+        return address(tokenContract);
+    }
+
+    /**
      * @notice Returns the URI corresponding to `stallName_`
      * @param stallName_ Stall name whose URI is being queried
      * @return the URI pointing to the stall's metadata
@@ -47,17 +80,6 @@ contract Market is Context {
         require(stallNameTaken(stallName_), "Market: unregistered stall name");
 
         return _uris[stallName_];
-    }
-
-    /**
-     * @notice Returns the stall name registered to`vendor_` address
-     * @param vendor_ The account address to whom the stall belongs to
-     * @return the stall name registered to the vendor
-     */
-    function vendorStallName(address vendor_) external view returns (string memory) {
-        require(_vendorRegistered(vendor_), "Market: account does not own a stall");
-
-        return _stallNames[vendor_];
     }
 
     /**
@@ -78,8 +100,64 @@ contract Market is Context {
 
         _stallNames[_msgSender()] = stallName_;
         _uris[stallName_] = uri_;
+        _vendors[stallName_] = _msgSender();
 
         emit StallRegistration(_msgSender(), stallName_, uri_);
+    }
+
+    /**
+     * @notice Creates and posts a new token for sale
+     * @param uri_ The token metadata's URI
+     * @param supply_ Max supply for the token. Zero if the supply is unlimited
+     * @param price_ The token's price. Must be >= than `MINIMUM_NFT_PRICE`
+     */
+    function postTokenForSale(
+        string memory uri_, 
+        uint128 supply_,
+        uint256 price_
+    ) external onlyVendor {
+        require(
+            price_ >= MINIMUM_NFT_PRICE, 
+            string(abi.encodePacked(
+                "Market: price lower than minimum of ", 
+                Strings.toString(MINIMUM_NFT_PRICE))));
+
+        uint128 class = tokenContract.registerClass(uri_, supply_, _msgData());
+        _stallTokens[vendorStallName(_msgSender())].push(class);
+        _tokenPrices[class] = price_;
+    }
+
+    /**
+     * @notice Returns the list of token classes for sale in the given stall
+     * @param stallName_ Stall name in which the token classes are for sale
+     * @return an array of token classes for sale in the stall `_stall`
+     */
+    function stallTokens(string calldata stallName_) public view returns (uint128[] memory) {
+        require(stallNameTaken(stallName_), "Market: unregistered stall name");
+        
+        return _stallTokens[stallName_];
+    }
+
+    /**
+     * @notice Returns the stall name registered to`vendor_` address
+     * @param vendor_ The account address to whom the stall belongs to
+     * @return the stall name registered to the vendor
+     */
+    function vendorStallName(address vendor_) public view returns (string memory) {
+        require(_vendorRegistered(vendor_), "Market: account does not own a stall");
+
+        return _stallNames[vendor_];
+    }
+
+    /**
+     * @notice Returns the vendor address of the given stall `stallName_`
+     * @param stallName_ Stall name to query for vendor
+     * @return the stall owner's address
+     */
+    function stallVendor(string calldata stallName_) public view returns (address) {
+        require(stallNameTaken(stallName_), "Market: unregistered stall name");
+
+        return _vendors[stallName_];
     }
 
     /**
--- test/Market.ts
+++ test/Market.ts
@@ -3,20 +3,24 @@ import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { TransactionResponse } from "@ethersproject/providers";
 
-import type { Market } from "../typechain";
+import { Market, MultiToken, MultiToken__factory } from "../typechain";
+import { BigNumber } from "@ethersproject/bignumber";
 
 describe("Market contract", () => {
-  const TEST_STALL_NAME_1: string = "testStallName1";
+  const STALL_NAME_REGISTERED: string = "testStallName1";
+  const STALL_NAME_UNREGISTERED: string = "testStallName2";
   const TEST_URI_1: string = "testURI1";
-  const TEST_STALL_NAME_2: string = "testStallName2";
   const TEST_URI_2: string = "testURI2";
+  const TEST_SUPPLY_1: number = 10;
+  const TEST_PRICE_WEI: number = 1;
 
   let marketContract: Market;
+  let tokenContract: MultiToken;
   let defaultAddress: SignerWithAddress;
-  let signer2: SignerWithAddress;
+  let vendor: SignerWithAddress;
 
   beforeEach(async () => {
-    [defaultAddress, signer2] = await ethers.getSigners();
+    [defaultAddress, vendor] = await ethers.getSigners();
 
     const marketFactory = await ethers.getContractFactory(
       "Market",
@@ -25,20 +29,27 @@ describe("Market contract", () => {
     marketContract = await marketFactory.deploy();
     await marketContract.deployed();
 
+    const tokenFactory: MultiToken__factory = await ethers.getContractFactory(
+      "MultiToken"
+    );
+    tokenContract = tokenFactory.attach(
+      await marketContract.tokenContractAddress()
+    );
+
     await marketContract
-      .connect(signer2)
-      .registerStall(TEST_STALL_NAME_1, TEST_URI_1);
+      .connect(vendor)
+      .registerStall(STALL_NAME_REGISTERED, TEST_URI_1);
   });
 
   describe("registerStall", async () => {
     it("Should register stall successfully", async () => {
       const tx: TransactionResponse = await marketContract.registerStall(
-        TEST_STALL_NAME_2,
+        STALL_NAME_UNREGISTERED,
         TEST_URI_2
       );
       await expect(tx)
         .to.be.emit(marketContract, "StallRegistration")
-        .withArgs(defaultAddress.address, TEST_STALL_NAME_2, TEST_URI_2);
+        .withArgs(defaultAddress.address, STALL_NAME_UNREGISTERED, TEST_URI_2);
     });
 
     it("Should fail due to empty stall name", async () => {
@@ -49,41 +60,104 @@ describe("Market contract", () => {
 
     it("Should fail due to empty URI", async () => {
       await expect(
-        marketContract.registerStall(TEST_STALL_NAME_2, "")
+        marketContract.registerStall(STALL_NAME_UNREGISTERED, "")
       ).to.be.revertedWith("Market: empty metadata URI");
     });
 
-    it("Should fail due to already registered vendro", async () => {
+    it("Should fail due to already registered vendor", async () => {
       await expect(
         marketContract
-          .connect(signer2)
-          .registerStall(TEST_STALL_NAME_2, TEST_URI_2)
+          .connect(vendor)
+          .registerStall(STALL_NAME_UNREGISTERED, TEST_URI_2)
       ).to.be.revertedWith("Market: account already owns a stall");
     });
 
     it("Should fail due to already registered stall name", async () => {
       await expect(
-        marketContract.registerStall(TEST_STALL_NAME_1, TEST_URI_2)
+        marketContract.registerStall(STALL_NAME_REGISTERED, TEST_URI_2)
       ).to.be.revertedWith("Market: stall name already taken");
+    });
+  });
+
+  describe("postTokenForSale", async () => {
+    it("Should emit ClassRegistration when posting token for sale", async () => {
+      const REGISTERED_CLASS: number = 1;
+
+      await expect(
+        marketContract
+          .connect(vendor)
+          .postTokenForSale(TEST_URI_1, TEST_SUPPLY_1, TEST_PRICE_WEI)
+      )
+        .to.emit(tokenContract, "ClassRegistration")
+        .withArgs(REGISTERED_CLASS, TEST_URI_1, TEST_SUPPLY_1);
+    });
+
+    it("Should fail due to unregistered vendor", async () => {
+      await expect(
+        marketContract.postTokenForSale(
+          TEST_URI_1,
+          TEST_SUPPLY_1,
+          TEST_PRICE_WEI
+        )
+      ).to.be.revertedWith("Market: account is not a registered vendor");
+    });
+
+    it("Should fail due to under price", async () => {
+      await expect(
+        marketContract
+          .connect(vendor)
+          .postTokenForSale(TEST_URI_1, TEST_SUPPLY_1, ethers.constants.Zero)
+      ).to.be.revertedWith("Market: price lower than minimum of 1");
     });
   });
 
   describe("uri", async () => {
     it("Should get correct URI", async () => {
-      expect(await marketContract.uri(TEST_STALL_NAME_1)).to.equals(TEST_URI_1);
+      expect(await marketContract.uri(STALL_NAME_REGISTERED)).to.equals(
+        TEST_URI_1
+      );
     });
 
     it("Should fail due to inexistent stall name", async () => {
-      await expect(marketContract.uri(TEST_STALL_NAME_2)).to.be.revertedWith(
-        "Market: unregistered stall name"
-      );
+      await expect(
+        marketContract.uri(STALL_NAME_UNREGISTERED)
+      ).to.be.revertedWith("Market: unregistered stall name");
+    });
+  });
+
+  describe("stallTokens", async () => {
+    it("Should return empty array", async () => {
+      expect(await marketContract.stallTokens(STALL_NAME_REGISTERED)).to.be
+        .empty;
+    });
+
+    it("Should return list of tokens for sale in stall", async () => {
+      const classes: Array<BigNumber> = [];
+
+      for (let i = 0; i < 10; i++) {
+        await marketContract
+          .connect(vendor)
+          .postTokenForSale(TEST_URI_1, TEST_SUPPLY_1, TEST_PRICE_WEI);
+
+        classes.push(BigNumber.from(i + 1));
+      }
+
+      expect(
+        await marketContract.stallTokens(STALL_NAME_REGISTERED)
+      ).to.deep.equal(classes);
+    });
+
+    it("Should fail due to unregistered stall name", async () => {
+      await expect(
+        marketContract.stallTokens(STALL_NAME_UNREGISTERED)
+      ).to.be.revertedWith("Market: unregistered stall name");
     });
   });
 
   describe("vendorStallName", async () => {
     it("Should get correct stall name", async () => {
-      expect(await marketContract.vendorStallName(signer2.address)).to.equals(
-        TEST_STALL_NAME_1
+      expect(await marketContract.vendorStallName(vendor.address)).to.equals(
+        STALL_NAME_REGISTERED
       );
     });
 
@@ -94,17 +168,31 @@ describe("Market contract", () => {
     });
   });
 
-  describe("stallNameTaken", async () => {
-    it("Should return true", async () => {
-      expect(await marketContract.stallNameTaken(TEST_STALL_NAME_1)).to.equals(
-        true
+  describe("stallVendor", async () => {
+    it("Should return stall vendor address", async () => {
+      expect(await marketContract.stallVendor(STALL_NAME_REGISTERED)).to.equals(
+        vendor.address
       );
     });
 
+    it("Should fail due to unregistered stall name", async () => {
+      await expect(
+        marketContract.stallVendor(STALL_NAME_UNREGISTERED)
+      ).to.be.revertedWith("Market: unregistered stall name");
+    });
+  });
+
+  describe("stallNameTaken", async () => {
+    it("Should return true", async () => {
+      expect(
+        await marketContract.stallNameTaken(STALL_NAME_REGISTERED)
+      ).to.equals(true);
+    });
+
     it("Should return false", async () => {
-      expect(await marketContract.stallNameTaken(TEST_STALL_NAME_2)).to.equals(
-        false
-      );
+      expect(
+        await marketContract.stallNameTaken(STALL_NAME_UNREGISTERED)
+      ).to.equals(false);
     });
   });
 });
